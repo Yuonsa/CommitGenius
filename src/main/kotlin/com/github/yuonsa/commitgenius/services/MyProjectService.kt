1b54daa155f6035c9bb467e00717bd050e673d83
GitHub Action <action@github.com>
2026-05-21 05:38:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/yuonsa/commitgenius/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yuonsa/commitgenius/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.yuonsa.commitgenius.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yuonsa.commitgenius.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
